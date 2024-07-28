2cba6c5fc2824fc03f558fd940c8e4c2ebb2b209
Hoàng Văn Đại <hoangdai06n@gmail.com>
2024-07-28 19:34:05 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class Main extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
         setContentView(R.layout.activity_main);
 
         openApp("com.example.myapp"); // Thay "com.example.myapp" bằng tên gói của ứng dụng mà bạn muốn mở
